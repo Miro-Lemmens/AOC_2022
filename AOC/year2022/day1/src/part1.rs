befded6796a5a1b2fcdda1b78a3ad43cfa0b4bcf
Mirre23 <Lemmensmiro@gmail.com>
2022-12-01 09:09:56 +0100

    day1 converted to 1 file

--- AOC/year2022/day1/src/part1.rs
+++ AOC/year2022/day1/src/part1.rs
@@ -1,7 +1,6 @@
 use lib::read_lines;
 
-fn main() {
-    let lines = read_lines();
+fn create_elfs_list(lines: Vec::<String>) -> Vec::<i32> {
     let mut elfs = Vec::new();
     let mut elf  = 0;
     for cal in lines {
@@ -14,11 +13,27 @@ fn main() {
                 .unwrap();
         }
     }
+    return elfs
+}
 
+fn part1(elfs: Vec::<i32>) -> i32 {
     let max_cal_elf = elfs
         .iter()
         .max()
         .unwrap();
 
-    println!("{}", max_cal_elf)
+    return *max_cal_elf
+}
+
+fn part2(mut elfs: Vec::<i32>) -> i32 {
+    elfs.sort();
+    let total_cal = elfs[elfs.len() - 1] + elfs[elfs.len() - 2] + elfs[elfs.len() - 3];
+    return total_cal
+}
+
+fn main() {
+    let lines = read_lines();
+    let elfs_list = create_elfs_list(lines);
+    println!("{}", part1(elfs_list.clone()));
+    println!("{}", part2(elfs_list.clone()));
 }
