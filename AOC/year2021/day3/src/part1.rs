57d0b73606ccde86d02b81cb3ea7b50547d695af
Mirre23 <Lemmensmiro@gmail.com>
2022-12-01 12:11:30 +0100

    year 2021 day3 semi-fixed

--- AOC/year2021/day3/src/part1.rs
+++ AOC/year2021/day3/src/part1.rs
@@ -1,35 +1,70 @@
 use lib::read_lines;
+use std::mem::replace;
 
 fn main() {
     let lines = read_lines();
-    let mut gamma = Vec::<i32>::new();
-    let mut epsilon = Vec::<i32>::new();
+    let mut gamma = Vec::<Vec::<i32>>::new();
+    let mut epsilon = Vec::<Vec::<i32>>::new();
     for line in lines {
-        let mut column = Vec::<char>::new();
+        let mut row = Vec::<char>::new();
         for char in line.chars() {
-            column.push(char);
+            row.push(char);
         }
-        let mut one = 0;
-        for char in &column {
-            if char == &'1' {
-                one += 1
+        let pushrow = row
+            .iter()
+            .map(|c| *c as i32)
+            .map(|c| { c - 48 })
+            .collect::<Vec::<i32>>();
+
+        gamma.push(pushrow.clone());
+
+        for mut num in 0..pushrow.len() {
+            if num == 0 {
+                let _ = replace(&mut num, 1);
+            } else {
+                let _ = replace(&mut num, 0);
             }
         }
-        if one > (column.len()/2) {
-            gamma.push(1);
-            epsilon.push(0);
-        } else {
-            gamma.push(0);
-            epsilon.push(1)
-        }
+        epsilon.push(pushrow.clone());
     }
 
-    println!("{}", )
-    let mut gamma_int = 0;
-    let mut epsilon_int = 0;
-    for i in gamma.len()..0 {
-        gamma_int += (2_i32.pow(i.try_into().unwrap()))*gamma[i];
-        epsilon_int += (2_i32.pow(i.try_into().unwrap()))*epsilon[i];
+    let gamma_f = create_2d_vec(gamma);
+    let gamma_b = gamma_f
+        .iter()
+        .map(|c| amt_to_bin(*c))
+        .collect::<Vec<i32>>();
+
+    let epsilon_f = create_2d_vec(epsilon);
+    let epsilon_b = epsilon_f
+        .iter()
+        .map(|c| amt_to_bin(*c))
+        .collect::<Vec<i32>>();
+
+    println!("{}", bin_vec_to_dec(gamma_b) * bin_vec_to_dec(epsilon_b));
+
+}
+
+fn amt_to_bin(num: i32) -> i32 {
+    let k = (1000 / num)-1;
+    return k
+}
+
+fn bin_vec_to_dec(vector: Vec<i32>) -> i32 {
+    let mut r = 0;
+    for i in 0..vector.len() {
+        r += 2_i32.pow(i as u32) * vector[i];
     }
-    println!("{}", gamma_int*epsilon_int);
+    return r
+}
+
+fn create_2d_vec(vector: Vec::<Vec::<i32>>) -> Vec::<i32> {
+    let mut f = <Vec::<i32>>::new();
+    for i in 0..vector[0].len() {
+        let mut gamma_temp = 0;
+        for row in vector.clone() {
+            gamma_temp += row[i];
+        }
+        f.push(gamma_temp);
+    }
+    return f
 }
